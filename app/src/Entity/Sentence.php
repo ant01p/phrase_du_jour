f0e6eff21b8809b9d886faa6d788afdfd2189e8b
ant01p <ponvienne.antoine@gmail.com>
2026-04-12 15:46:51 +0200

    add Entity Comment

--- app/src/Entity/Sentence.php
+++ app/src/Entity/Sentence.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SentenceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SentenceRepository::class)]
@@ -25,6 +27,17 @@ class Sentence
     #[ORM\ManyToOne(inversedBy: 'sentences')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
+
+    /**
+     * @var Collection<int, Comment>
+     */
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'sentence', orphanRemoval: true)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -75,6 +88,36 @@ class Sentence
     public function setCategory(?Category $category): static
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setSentence($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getSentence() === $this) {
+                $comment->setSentence(null);
+            }
+        }
 
         return $this;
     }
