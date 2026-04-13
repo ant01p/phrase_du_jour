66642bedfd003aacdf3418e8db84e0e99ba667af
ant01p <ponvienne.antoine@gmail.com>
2026-04-13 13:53:32 +0200

    display/sentence-admin

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SentenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(SentenceRepository $sentences): Response
     {
+        $sentences = $sentences->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'sentences' => $sentences,
         ]);
     }
+
 }
